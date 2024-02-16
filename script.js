c15e727b2e8d41c57ba33bb5d36531e1297ee30d
Sheen Ismhael Lim <sheenismhael.lim@outlook.ph>
2024-02-16 02:22:59 +0000

    added functionality for dark mode

---
+++ script.js
@@ -0,0 +1,11 @@
+const toogleSwitch = document.querySelector("input[type='checkbox'");
+
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+}
+
+toogleSwitch.addEventListener("change", switchTheme);
